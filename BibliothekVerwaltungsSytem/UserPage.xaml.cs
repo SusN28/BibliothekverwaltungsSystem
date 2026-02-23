0b7c262c4196f3f64e4426e5469029b993878dd0
SusN28 <ngeorgiev843@yahoo.com>
2026-02-23 14:08:58 +0100

    User wird jetzt mit seinem vollen Namen begrüßt wenn er sich anmeldet !

--- BibliothekVerwaltungsSytem/UserPage.xaml.cs
+++ BibliothekVerwaltungsSytem/UserPage.xaml.cs
@@ -5,10 +5,13 @@ namespace BibliothekVerwaltungsSytem
 {
     public partial class UserPage : Page
     {
-        
         public UserPage()
         {
             InitializeComponent();
+
+            // Vorname und Nachname aus der Session laden
+            if (Session.CurrentUser != null)
+                TxtWillkommen.Text = $"Willkommen, {Session.CurrentUser.Vorname} {Session.CurrentUser.Nachname}!";
         }
 
         private void WeiterButton_Click(object sender, RoutedEventArgs e)
